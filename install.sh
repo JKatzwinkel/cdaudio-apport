84ca26ea01deb6edc5ef85ac267da61eaa035549
J. Katzwinkel <thor@mintj>
2013-06-07 12:39:03 +0200

    some validation in install script

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
 #/bin/bash
 
+home=$PWD
+
 sudo apt-get install gcc git libcdio-cdda-dev libcdio-paranoia-dev
-git clone git://github.com/tuffy/python-audio-tools.git
+if [ ! -d python-audio-tools ]; then
+ git clone git://github.com/tuffy/python-audio-tools.git
+fi
 cd python-audio-tools
 python setup.py build
-cp -r build/lib*/audiotools ..
-cd ..
+if [ ! -d $home/audiotools ]; then
+ cp -r build/lib*/audiotools $home
+fi
+cd $home
 
 if [ ! -f tcl.tar.gz ]; then
  wget http://prdownloads.sourceforge.net/tcl/tcl8.6.0-src.tar.gz -O tcl.tar.gz
@@ -16,19 +22,17 @@ fi
 tar -xzf tcl.tar.gz
 tar -xzf tk.tar.gz
 
-target=$PWD
 cd tcl*
 cd unix
-./configure --prefix=$target --exec-prefix=$target
+./configure --prefix=$home --exec-prefix=$home
 make
 make install
-cd $target
+cd $home
 
 cd tk*
 cd unix
-./configure --prefix=$target --exec-prefix=$target
+./configure --prefix=$home --exec-prefix=$home
 make
 make install
-cd $target
-
+cd $home
 
