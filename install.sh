bcfe0f78d5d9ce0e3103613580fdaa9b35cb9456
J. Katzwinkel <thor@mintj>
2013-06-07 13:25:54 +0200

    Tkinter comes with standard python distro, as it turns out

--- install.sh
+++ install.sh
@@ -13,26 +13,37 @@ if [ ! -d $home/audiotools ]; then
 fi
 cd $home
 
-if [ ! -f tcl.tar.gz ]; then
- wget http://prdownloads.sourceforge.net/tcl/tcl8.6.0-src.tar.gz -O tcl.tar.gz
-fi
-if [ ! -f tk.tar.gz ]; then
- wget http://prdownloads.sourceforge.net/tcl/tk8.6.0-src.tar.gz -O tk.tar.gz
-fi
-tar -xzf tcl.tar.gz
-tar -xzf tk.tar.gz
+#if [ ! -f tcl.tar.gz ]; then
+# if [ ! -d tcl8.6.0 ]; then
+#  wget http://prdownloads.sourceforge.net/tcl/tcl8.6.0-src.tar.gz -O tcl.tar.gz
+# fi
+#fi
+#if [ ! -f tk.tar.gz ]; then
+# if [ ! -d tk8.6.0 ]; then
+#  wget http://prdownloads.sourceforge.net/tcl/tk8.6.0-src.tar.gz -O tk.tar.gz
+# fi
+#fi
+#
+#if [ ! -d tcl8.6.0 ]; then
+# tar -xzf tcl.tar.gz
+#fi
+#if [ ! -d tk8.6.0 ]; then
+# tar -xzf tk.tar.gz
+#fi
+#
+#inst=$home/local
+#cd tcl*
+#cd unix
+#./configure --prefix=$inst --exec-prefix=$inst
+#make
+#make install
+#cd $home
+#
+#cd tk*
+#cd unix
+#./configure --prefix=$inst --exec-prefix=$inst --with-tcl=$inst/tcl8.6.0/unix
+#make
+#make install
+#cd $home
 
-cd tcl*
-cd unix
-./configure --prefix=$home --exec-prefix=$home
-make
-make install
-cd $home
-
-cd tk*
-cd unix
-./configure --prefix=$home --exec-prefix=$home
-make
-make install
-cd $home
-
+chmod +x start
