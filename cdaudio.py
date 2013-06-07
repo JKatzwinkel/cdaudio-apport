80a1e05ede86f933556fd821198de4368018bf3d
J. Katzwinkel <thor@mintj>
2013-06-07 15:29:15 +0200

    damn gui!

--- cdaudio.py
+++ cdaudio.py
@@ -2,39 +2,104 @@
 
 import sys
 from optparse import OptionParser
-import TKinter as gui
+import Tkinter as gui
+import time
+import os.path
+
+
+class Gui():
+	def __init__(self, root):
+		#super(Gui, self).__init__()
+		self.root = root
+		self.root.title('Audio CD')
+		screen = (self.root.winfo_screenwidth(), self.root.winfo_screenheight())
+		width  = int(screen[0] * .8)
+		height  = int(screen[1] * .8)
+		posX   = (screen[0] - width)/2 # Find left and up border of window
+		posY   = (screen[1] - height)/2
+		self.root.geometry("%sx%s+%s+%s"%(width,height,posX,posY))
+		#self.titlebar=gui.Label(self.root, text="No CD").grid(row=0)
+		self.frame=gui.Frame(self.root)
+		self.frame.grid(row=1)
+		self.bottom=gui.Frame(self.root).grid(row=2)
+		self.status=gui.Label(self.bottom, text="waiting...")
+		self.status.pack()
+		self.items=[]
+
+	def add_tracks(self, tracks):
+		for i, filename in enumerate(tracks):
+			checked = gui.IntVar()
+			checked.set(1)
+			c=gui.Checkbutton(self.frame, text=filename, variable=checked)
+			c.grid(row=i, sticky=gui.W)
+		self.status.text="album"
+		#self.frame.grid(row=1)
+
+
 
 sys.path.append('python-audio-tools')
 
 import audiotools
 import audiotools.accuraterip
 
-cd = audiotools.CDDA("/dev/cdrom")
+cd_drive=os.path.realpath('/dev/cdrom')
+def cd_ready():
+	try:
+		if open(cd_drive):
+			return True
+	except:
+		return False
+	return False
 
-print dir(cd)
+
+# wait until drive is ready
+while not cd_ready():
+	time.sleep(5)
+
+cd = audiotools.CDDA("/dev/cdrom")
 meta = cd.metadata_lookup()
 
-print dir(cd[1])
 
 names=[]
 for i, track in enumerate(cd):
 	md = meta[0][i]
-	#print track.offset(), track.length(), md.track_number, md.track_name,
-	#print md.artist_name, md.album_name
 	filename = '{1} - {0} - {2}.mp3'.format(md.track_number, md.artist_name, md.track_name)
 	names.append(filename)
 
 
-GUI=gui.Tk()
-label=gui.Label(GUI, meta[0][0].album_name)
-label.pack()
-checked=[]
+win = gui.Tk()
+GUI = Gui(win)
+GUI.add_tracks(names)
+gui.mainloop()
 
-for name in names:
-	checked.append(gui.Variable())
-	c=gui.Checkbutton(GUI, text=name, variable=checked[-1])
+#GUI=gui.Tk()
+#GUI.title('Audio CD extraction')
+#label=gui.Label(GUI, text=meta[0][0].album_name)
+#label.pack()
 
-GUI.mainloop()
+#trackframe=gui.Frame(GUI)
+#trackframe.pack_propagate(0)
+#trackframe.width=600
+#trackframe.borderwidth=2
+#checked=[]
+
+#for i, name in enumerate(names):
+#	checked.append(gui.IntVar())
+#	checked[-1].set(1)
+#	c=gui.Checkbutton(trackframe, text=name, variable=checked[-1])
+#	c.grid(row=i, sticky=gui.W)
+	#c.pack(fill=gui.X, side=gui.TOP)
+
+#trackframe.pack()
+	
+#statusbar=gui.Frame(GUI, bg='#b0d0ff')
+#statusbar.pack_propagate(0)
+#statusbar.borderwidth=1
+#statusbar.width=trackframe.width
+#statusbar.height=30
+#statusbar.pack(fill=gui.X, side=gui.BOTTOM)
+
+sys.exit(0)
 
 def update(current, total):
 	print '\r', current, total
